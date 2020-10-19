51b9fd00b6b11bf86a0b768aadb0ecf715339858
abdel <wahab-38@hotmail.fr>
2020-10-19 09:12:22 +0200

    affichage fonctionnel

---
+++ src/Entity/Vente.php
@@ -0,0 +1,262 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VenteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VenteRepository::class)
+ */
+class Vente
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $denommination;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $photo;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $surface;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $typemaison;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $chambres;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $etage;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cout;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $codepostal;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $accessibilite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getDenommination(): ?string
+    {
+        return $this->denommination;
+    }
+
+    public function setDenommination(string $denommination): self
+    {
+        $this->denommination = $denommination;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSurface(): ?int
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(int $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getTypemaison(): ?string
+    {
+        return $this->typemaison;
+    }
+
+    public function setTypemaison(string $typemaison): self
+    {
+        $this->typemaison = $typemaison;
+
+        return $this;
+    }
+
+    public function getChambres(): ?int
+    {
+        return $this->chambres;
+    }
+
+    public function setChambres(int $chambres): self
+    {
+        $this->chambres = $chambres;
+
+        return $this;
+    }
+
+    public function getEtage(): ?int
+    {
+        return $this->etage;
+    }
+
+    public function setEtage(int $etage): self
+    {
+        $this->etage = $etage;
+
+        return $this;
+    }
+
+    public function getCout(): ?int
+    {
+        return $this->cout;
+    }
+
+    public function setCout(int $cout): self
+    {
+        $this->cout = $cout;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCodepostal(): ?int
+    {
+        return $this->codepostal;
+    }
+
+    public function setCodepostal(int $codepostal): self
+    {
+        $this->codepostal = $codepostal;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getAccessibilite(): ?string
+    {
+        return $this->accessibilite;
+    }
+
+    public function setAccessibilite(string $accessibilite): self
+    {
+        $this->accessibilite = $accessibilite;
+
+        return $this;
+    }
+}
