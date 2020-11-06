18982aa568ec6d9d09a9c7f5320a9d2e26471b88
Abdelwahab Baghezza <wahab-38@hotmail.fr>
2020-11-06 11:50:03 +0100

    forms

---
+++ src/Form/GuinotLocationFormType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\GuinotLocation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GuinotLocationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('createdAt')
+            ->add('denomination')
+            ->add('categorie')
+            ->add('photo')
+            ->add('description')
+            ->add('surface')
+            ->add('type_maison')
+            ->add('chambre')
+            ->add('etage')
+            ->add('cout')
+            ->add('adresse')
+            ->add('accessibilite')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => GuinotLocation::class,
+        ]);
+    }
+}
